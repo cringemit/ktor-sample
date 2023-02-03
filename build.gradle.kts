ddf0109ae19d9b2eb4b734041a885cb83597f4bb
rainyrobs <raintown-code@proton.me>
2023-02-03 23:55:37 +0000

    Changed to HOCON configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ plugins {
 group = "com.raintown"
 version = "0.0.1"
 application {
-    mainClass.set("com.raintown.ApplicationKt")
+    mainClass.set("io.ktor.server.netty.EngineMain")
 
     val isDevelopment: Boolean = project.ext.has("development")
     applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
--- src/main/kotlin/com/raintown/Application.kt
+++ src/main/kotlin/com/raintown/Application.kt
@@ -1,16 +1,14 @@
 package com.raintown
 
 import io.ktor.server.application.*
-import io.ktor.server.engine.*
-import io.ktor.server.netty.*
 import com.raintown.plugins.*
 
-fun main() {
-    embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
-        .start(wait = true)
-}
+fun main(args: Array<String>): Unit =
+    io.ktor.server.netty.EngineMain.main(args)
 
+@Suppress("unused") // application.conf references the main function. This annotation prevents the IDE from marking it as unused.
 fun Application.module() {
     configureSerialization()
     configureRouting()
 }
+
