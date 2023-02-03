e68277988f6606e1c162b38a7d7edb15ec783b75
rainyrobs <raintown-code@proton.me>
2023-02-03 23:21:46 +0000

    Customer Routing

--- src/main/kotlin/com/raintown/plugins/Routing.kt
+++ src/main/kotlin/com/raintown/plugins/Routing.kt
@@ -1,13 +1,11 @@
 package com.raintown.plugins
 
+import com.routes.customerRouting
 import io.ktor.server.routing.*
-import io.ktor.server.response.*
 import io.ktor.server.application.*
 
 fun Application.configureRouting() {
     routing {
-        get("/") {
-            call.respondText("Hello World!")
-        }
+        customerRouting()
     }
 }
