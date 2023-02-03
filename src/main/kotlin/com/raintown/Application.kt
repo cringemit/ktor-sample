57f1403d0f814efec40ddf530ba5787ecba06a68
rainyrobs <raintown-code@proton.me>
2023-02-03 14:03:58 +0000

    Empty Ktor project

---
+++ src/main/kotlin/com/raintown/Application.kt
@@ -0,0 +1,15 @@
+package com.raintown
+
+import io.ktor.server.application.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import com.raintown.plugins.*
+
+fun main() {
+    embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
+        .start(wait = true)
+}
+
+fun Application.module() {
+    configureRouting()
+}
